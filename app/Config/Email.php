8ac302d45bf414f6d47759354414c150e2c3e944
zurakotaro-24 <calingjared0224@gmail.com>
2026-03-24 16:41:42 +0800

    Finished email function, not tested

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -48,12 +48,12 @@ class Email extends BaseConfig
     /**
      * SMTP Port
      */
-    public int $SMTPPort = 25;
+    public int $SMTPPort = 587;
 
     /**
      * SMTP Timeout (in seconds)
      */
-    public int $SMTPTimeout = 5;
+    public int $SMTPTimeout = 60;
 
     /**
      * Enable persistent SMTP connections
@@ -82,7 +82,7 @@ class Email extends BaseConfig
     /**
      * Type of mail, either 'text' or 'html'
      */
-    public string $mailType = 'text';
+    public string $mailType = 'html';
 
     /**
      * Character set (utf-8, iso-8859-1, etc.)
--- app/Controllers/TestMailController.php
+++ app/Controllers/TestMailController.php
@@ -4,11 +4,38 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
+use Config\Services;
 
 class TestMailController extends BaseController
 {
     public function index()
     {
+        $to = 'sample@gmail.com';
+        $subject = 'subject ng email to par';
+        $message = 'Eto naman ang laman. <br>Nakaset gamit html dito sa App/Config/Email.php';
+
+        $email = Services::email();
+        $email->setTo($to);
+        $email->setFrom('something@email.com', 'Your Name');
+
+        // BCC - Blind Carbon Copy - sends a copy of the sent email to that account, invisible to the receiver.
+        // $email->setBCC('somebcc@email.com');
+        // CC - Carbon Copy - sends a copy of the sent email to that account, visible to the receiver.
+        // $email->setCC(['somecc@email.com', 'somecc2@email.com']);
+
+        $email->setSubject($subject);
+        $email->setMessage($message);
         
+        $filepath = 'public\assets\images\page_banner.jpg';
+        $email->attach($filepath);
+        if($email->send())
+        {
+            echo "Eyy nagsend sya par";
+        }
+        else
+        {
+            $data = $email->printDebugger(['headers']);
+            print_r($data);
+        }
     }
 }
